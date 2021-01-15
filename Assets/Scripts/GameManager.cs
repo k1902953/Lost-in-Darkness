a5900473b3df1945a4a3d05dbe8364f51de2eaeb
k1902953 <64602858+k1902953@users.noreply.github.com>
2021-01-15 01:26:14 +0000

    15/02
    canvas, paper, redo terrain, colliders

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,93 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public TMP_Text batterytxt;        // Text field for the batteries collected
+    public TMP_Text papertxt;          // Text field for the papers collected
+    public TMP_Text gameovertxt;       // Text field for game over text
+    public TMP_Text healthtxt;         // Text field for the health text
+
+    public int health = 100;           // the player health
+    public bool gameover = false;     // a boolean to indicate the game over state
+
+    public int totalbatts = 0;       // Total number of batteries to collect
+    public int battscollected = 0;   // Number of batteries collected so far
+
+    public int totalpapers = 8;       // Total number of papers to collect
+    public int papercollected = 0;   // Number of paper collected so far
+
+    public static GameManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    // GameManager instance
+    private static GameManager instance = null;
+
+    void Awake()
+    {
+        if (instance)
+        {
+            Debug.Log("already an instance so destroying new one");
+            DestroyImmediate(gameObject);
+            return;
+        }
+
+        instance = this;
+
+        DontDestroyOnLoad(gameObject);
+    }
     // Start is called before the first frame update
     void Start()
     {
         
     }
+
+    public void pickup()
+    {
+        // Check that not game over
+        if (gameover) return;
+
+        // Add one to total number of keys collected
+        battscollected++;
+
+        // Update the text field to display the total number of collected keys in the form "Keys: ??"
+        batterytxt.text = "Battries: " + battscollected;
+    }
+
+    // goalreached() should set the gameover to true, and set the game over
+    // text to "GAME OVER!"
+    public void goalreached()
+    {
+        setGameOver();
+    }
+
+    // Is it game over
+    public bool isGameOver()
+    {
+        return gameover;
+    }
+
+    // Set the game over state to true
+    public void setGameOver()
+    {
+        gameover = true;
+        gameovertxt.text = "GAME OVER!";
+    }
+
+    // Set the health text
+    public void setHealthText(int health)
+    {
+        healthtxt.text = "Health: " + health;
+    }
+
 
     // Update is called once per frame
     void Update()
---
+++ Assets/Scripts/PickupTrigger.cs
@@ -0,0 +1,14 @@
+﻿// This is attached to each key, and is used to detecting if the player is overalapping the key
+
+using UnityEngine;
+using System.Collections;
+
+public class PickupTrigger : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider col)
+    {
+        GameManager.Instance.pickup();
+        Destroy(transform.parent.gameObject);
+    }
+}
